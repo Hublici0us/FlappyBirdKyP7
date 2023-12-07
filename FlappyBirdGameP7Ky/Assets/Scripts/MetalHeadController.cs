cfa56af89ebba6f781f613f6994eb124e2672464
Jason Ky <744169@student.dusd.net>
2023-12-06 22:53:58 -0800

    started animating the sprite
    I finished the pixels and started animating them with triggers.

--- FlappyBirdGameP7Ky/Assets/Scripts/MetalHeadController.cs
+++ FlappyBirdGameP7Ky/Assets/Scripts/MetalHeadController.cs
@@ -6,14 +6,16 @@ public class MetalHeadController : MonoBehaviour
 {
     public float upForce = 200f;
     bool isDead = false;
-    Rigidbody rb2d;
+    Rigidbody2D rb2d;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         /*at start, checks if rigidbody is attached to script
          * so we can later use it in parts of the script. */
-        rb2d = GetComponent<Rigidbody>();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,7 +27,14 @@ public class MetalHeadController : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Fly");
             }
         }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
     }
 }
