ffab8e33b099f87c91a97982a973cf3d74f07d39
Jason Ky <744169@student.dusd.net>
2023-12-12 15:29:53 -0800

    error with the sprites
    imported some sprites and theyre wroing ewie

--- FlappyBirdGameP7Ky/Assets/Scripts/GameControl.cs
+++ FlappyBirdGameP7Ky/Assets/Scripts/GameControl.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
 
     // Start is called before the first frame update
     void Awake()
@@ -28,7 +30,7 @@ public class GameControl : MonoBehaviour
     {
         if (gameOver == true && Input.GetKeyDown(KeyCode.Space)) 
         {
-            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
